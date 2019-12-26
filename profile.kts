d4deb0e8efdecdded0e68b50a4b97f83c1581fe9
David <32607418+TodFoxBR@users.noreply.github.com>
2019-12-26 17:33:46 -0300

    Adicionado novas respostas automáticas
    Resposta automática para dúvidas sobre a DJ Loritta, badges do perfil e sobre temas/fundos do comando de perfil

---
+++ profile.kts
@@ -0,0 +1,13 @@
+class ProfileBackgroundResponse : RegExResponse() {
+    init {
+        patterns.add("como".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("coloc[ar|a|o]|mud[a|o]|troc[a|o]|alter[a|o]".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("banner|background|imagem|fundo|foto".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("perfil|profile".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("\\?".toPattern(Pattern.CASE_INSENSITIVE))
+
+        response = "{@mention}, se você quer mudar a imagem de fundo do seu perfil, digite \"+background\"! Caso você queira mudar o tema do seu perfil, entre em https://loritta.website/us/user/@me/dashboard/profiles"
+    }
+}
+
+ProfileBackgroundResponse()
